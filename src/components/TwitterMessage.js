6983cac4e1a9d5036aa1daabbca65ba8a8d52d5c
Ben Hayes <b.t.hayes@comcast.net>
2021-08-17 17:27:00 -0500

    commit tree c97e1e146c31cbf137eef4b432c48c87b733b797

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: '',
+    };
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      message: event.target.value,
+    })
   }
 
   render() {
+    let characterCount = () => {
+      return (280 - this.state.message.length);
+    }
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={(event) => this.handleOnChange(event)}/>
+        {characterCount()}/280 remaining
       </div>
     );
   }
